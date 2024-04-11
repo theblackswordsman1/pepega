07b2a8e63fdd2e5c45adfcfc390817153c90406a
Aron William Bechen <williambechen@gmail.com>
2024-04-11 17:11:04 +0200

    1st

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ikt206g24v_05.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
